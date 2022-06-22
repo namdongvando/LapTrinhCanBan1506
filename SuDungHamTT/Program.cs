1fe5e705b598348295afa23f96c75994c2c7a582
namdongvando <32663464+namdongvando@users.noreply.github.com>
2022-06-22 21:30:32 +0700

    Buoi4
    Vong lap

---
+++ SuDungHamTT/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+
+namespace SuDungHamTT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        private static void Menu()
+        {
+            // xuat ra man hinh
+            Console.WriteLine("======Menu=======");
+            Console.WriteLine("1. Su Dung Vong lap");
+            Console.WriteLine("2. Tim So Lon Nha");
+            Console.WriteLine("3. Tỏng cac so 1-> 1/n");
+            Console.WriteLine("4. Tinh N!");
+            Console.WriteLine("5. Tinh tong tu 1 -> N");
+            // cho nguoi dung chon 
+            string chon = Console.ReadLine();
+            switch (chon)
+            {
+                case "1":
+                    SuDungVongLap(); 
+                    break;
+                case "5":
+                    TinhTongToiN();
+                    break;
+                case "4":
+                    TinhNGiaiThua();
+                    break;
+                case "3":
+                    TinhTongDaySo();
+                    break;
+
+            }
+            
+        }
+
+        private static void TinhTongDaySo()
+        {
+            try
+            {
+                Console.WriteLine("Nhap So Nguyen N");
+                int n;
+                bool kt =
+                     int.TryParse(Console.ReadLine(), out n);
+                if (kt == false)
+                {
+                    // nguoi dung nhap sai
+                    throw new Exception("Chuỗi không đúng dịnh dạng");
+                }
+                // nhap đúng
+                if (n <= 0)
+                {
+                    throw new Exception("N Phải Lớn Hơn 0");
+                }
+                double tong = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    tong = tong + 1.0/i;
+                }
+                Console.WriteLine("N!={0}", tong);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// tính N!
+        /// </summary>
+        private static void TinhNGiaiThua()
+        {
+            try
+            {
+                Console.WriteLine("Nhap So Nguyen N");
+                int n;
+               bool kt =
+                    int.TryParse(Console.ReadLine(), out n);
+                if(kt == false)
+                {
+                    // nguoi dung nhap sai
+                    throw new Exception("Chuỗi không đúng dịnh dạng");
+                }
+                // nhap đúng
+                if (n <= 0)
+                {
+                    throw new Exception("N Phải Lớn Hơn 0");
+                }
+                int giaiThua = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    giaiThua = giaiThua * i;
+                }
+                Console.WriteLine("N!={0}", giaiThua);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+
+        private static void TinhTongToiN()
+        {
+            try
+            {
+                //Nhap so nguyen N
+                Console.WriteLine("Nhap So nguyen N");
+                int n = int.Parse(Console.ReadLine());
+                int tong = 0;
+                for (int i = 1; i <=n ; i++)
+                {
+                    tong = tong + i;
+                }
+                Console.WriteLine("Tong Toi N La: {0}", tong);
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(ex.Message);
+            }  
+        }
+
+        private static void SuDungVongLap()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Xin chao");
+                Console.WriteLine(i+1);
+            }
+        }
+    }
+}
---
+++ SuDungVongLapWhile/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace SuDungVongLapWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // VongLap1();
+            // XuatSoChan();
+            BangCuuChuong();
+        }
+
+        private static void BangCuuChuong()
+        {
+            for (int i = 2; i <= 9; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine("{0} x {2} = {1}", i, i * j,j);
+                } 
+            }  
+        }
+
+        private static void XuatSoChan()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+        }
+
+        private static void VongLap1()
+        {
+            int index = 0;
+            while (index < 100)
+            {
+                Console.WriteLine("Xin chao");
+                index++;
+            }
+
+        }
+    }
+}
